3c0cbc4bd96e1518780ce0ca1bb40ee64c6f10d8
Gilles Patrick Ngondo <ngondogillespatrick@gmail.com>
2024-02-16 20:48:18 -0500

    Init Repo

---
+++ src/main/java/com/gilles/model/Student.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gilles.model;
+
+/**
+ *
+ * @author gilles
+ */
+public class Student {
+    
+}
